cdff0fb03b2bca5ff0ea4345f49c042f6ebcf1a6
lxgion <60397382+lxgion@users.noreply.github.com>
2024-07-16 12:02:55 +0200

    initial commit

---
+++ constants.py
@@ -0,0 +1,12 @@
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
+
+dis_width = 400
+dis_height = 400
+
+snake_block = 10
+snake_speed = 15
